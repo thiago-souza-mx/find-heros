98d11e86fdb19207be6b2cfa213f7d81b30fb7d8
thiago-souza-mx <thiagoraouza@gmail.com>
2021-07-16 04:35:33 -0300

    set port 80

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const controller = require("./src/controllers");
 const app = express();
-const port = 80;
+const port = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
   res.send('API de super-heróis')
